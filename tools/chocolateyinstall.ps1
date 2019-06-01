181afd451542193b831d714293c0aad605ab6841
Andrei <n3rd4i@gmail.com>
2019-06-01 14:21:13 +0200

    initial version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installLocation = "$ENV:LocalAppData\Programs\StarCraft SOR"
+$shortcutPath = "$ENV:UserProfile\Desktop\SC SOR.lnk"
+$shortcutAIPath = "$ENV:UserProfile\Desktop\SC SOR AI.lnk"
+$starcraftLocation = "$ENV:LocalAppData\Programs\StarCraft"
+$SOR_EXE = "SOR 4.7.exe"
+$SOR_AI_EXE = "SOR 4.7-Ai.exe"
+$srcURL = 'https://www.moddb.com/downloads/start/176655'
+$tokenURL = '(https://www.moddb.com/downloads/mirror/176655/\w+/\w+)'
+$content = (Invoke-WebRequest $srcURL -UseBasicParsing).Content
+$url = (select-string -Input $content -Pattern $tokenURL).Matches[0]
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$installLocation"
+  url           = $url
+  softwareName  = 'starcraft-sor*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = '67DE15ECBFC42F103F180FBF867CD157'
+  checksumType  = 'md5' #default is md5, can also be sha1, sha256 or sha512
+}
+Install-ChocolateyZipPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+
+New-Item -itemtype symboliclink -force -path "$starcraftLocation" `
+  -name "$SOR_EXE" -value "$installLocation\$SOR_EXE"
+Install-ChocolateyShortcut -ShortcutFilePath "$shortcutPath" `
+  -TargetPath "$starcraftLocation\$SOR_EXE" `
+  -IconLocation "$installLocation\$SOR_EXE" `
+  -WorkingDirectory "$starcraftLocation"
+
+New-Item -itemtype symboliclink -force -path "$starcraftLocation" `
+  -name "$SOR_AI_EXE" -value "$installLocation\$SOR_AI_EXE"
+Install-ChocolateyShortcut -ShortcutFilePath "$shortcutAIPath" `
+  -TargetPath "$starcraftLocation\$SOR_AI_EXE" `
+  -IconLocation "$installLocation\$SOR_AI_EXE" `
+  -WorkingDirectory "$starcraftLocation"
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,37 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$installLocation = "$ENV:LocalAppData\Programs\StarCraft SOR"
+$shortcutPath = "$ENV:UserProfile\Desktop\SC SOR.lnk"
+$shortcutAIPath = "$ENV:UserProfile\Desktop\SC SOR AI.lnk"
+$starcraftLocation = "$ENV:LocalAppData\Programs\StarCraft"
+$SOR_EXE = "SOR 4.7.exe"
+$SOR_AI_EXE = "SOR 4.7-Ai.exe"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'starcraft-sor*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+}
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+if ($key.Count -eq 1) {
+  $key | % { 
+    $packageArgs['file'] = "$($_.UninstallString)" #NOTE: You may need to split this if it contains spaces, see below
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      $packageArgs['file'] = ''
+    } else {
+    }
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
+Remove-Item "$starcraftLocation\$SOR_EXE" -force
+Remove-Item "$starcraftLocation\$SOR_AI_EXE" -force
+Remove-Item $shortcutPath -force
+Remove-Item $shortcutAIPath -force
+Remove-Item $installLocation -recurse -force
